e493901384baf3d512170ebeeff7e6113a0092db
NLH-Gnext <gnext.nyanlinhtet@gmail.com>
2024-02-26 14:04:58 +0630

    messagedtos

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dtos';
 
-@Controller('messages')
-export class MessagesController {}
+@Controller('messages') //controller = class decorator
+export class MessagesController {
+  @Get() //get,post,etc = method decorator
+  listMessages() {}
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+    //body ,param = argument decorator
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
+}
