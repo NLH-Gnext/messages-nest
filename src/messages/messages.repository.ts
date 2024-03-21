32d98a73175c638d670aaccde5f28a0a0ccdd3a3
NLH-Gnext <gnext.nyanlinhtet@gmail.com>
2024-03-21 09:47:49 +0630

    first

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,10 +1,23 @@
+import { writeFile } from 'fs';
 import { readFile } from 'fs/promises';
 
 export class MessagesRepository {
   async findOne(id: string) {
     const content = await readFile('messages.json', 'utf8'); //json file တွေက pure string တွေဖြစ်
-    const messages = JSON.parse(content); 
+    const messages = JSON.parse(content);
+    return messages[id];
   }
-  async findAll() {}
-  async create(message: string) {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = { id, content };
+    // await writeFile('messages.json', JSON.stringify(messages));
+  }
 }
